faf18909e05b2cd9ab75ef95d6e4af726b8bcb40
Morgane Farge <morganefarge69@gmail.com>
2026-01-20 10:08:01 +0100

    Initialisation du frontend TicketFlow (React)

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>TicketFlow</h1>
+      <p>Mon projet TicketFlow démarre </p>
+    </div>
+  );
+}
+
---
+++ src/mockTickets.js
@@ -0,0 +1,5 @@
+export const mockTickets = [
+  { id: 1, title: "Bug login" },
+  { id: 2, title: "Améliorer le design" },
+  { id: 3, title: "Ajouter un filtre" }
+];
